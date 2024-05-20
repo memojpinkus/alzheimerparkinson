c31d0189cf0d59714536e730d47475d0a8c3924f
memojpinkus <memojpinkus@gmail.com>
2024-05-20 16:19:29 -0700

    Base layout
    Base layout

---
+++ src/constants.tsx
@@ -0,0 +1,36 @@
+import { Icon } from "@iconify/react";
+
+import { SideNavItem } from "./types";
+
+export const SIDENAV_ITEMS: SideNavItem[] = [ 
+    {
+        title: "Detection",
+        path: "/detection",
+        icon: <Icon icon="lucide:scan-line" width="24" height="24"/>,
+    },
+    {
+        title: "Get Started",
+        path: "/getstarted",
+        icon: <Icon icon="lucide:rocket" width="24" height="24"/>
+    },
+    {
+        title: "Dashboard",
+        path: "/",
+        icon: <Icon icon="lucide:home" width="24" height="24"/>,
+    },
+    {
+        title: 'Settings',
+        path: '/settings',
+        icon: <Icon icon="lucide:settings" width="24" height="24" />,
+        submenu: true,
+        subMenuItems: [
+          { title: 'Account', path: '/settings/account' },
+          { title: 'Privacy', path: '/settings/privacy' },
+        ],
+    },
+    {
+        title: 'Help',
+        path: '/help',
+        icon: <Icon icon="lucide:help-circle" width="24" height="24" />,
+    },
+]
