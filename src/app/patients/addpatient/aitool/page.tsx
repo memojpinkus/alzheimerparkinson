cad742bbf43fd33fa23ff20b30727ee5ec2a72ed
memojpinkus <memojpinkus@gmail.com>
2024-05-22 13:38:37 -0700

    CRUD
    Full CRUD functionality

---
+++ src/app/patients/addpatient/aitool/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+
+const GetStarted = () => {
+  return (
+    <>
+      <span className="font-bold text-4xl">Get Started</span>
+
+      <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg">Would you like to use detection AI tool right now?</div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg">
+        <Link href={"/patients"}>Yes</Link>
+        <Link href={"/patients"}>No</Link>
+      </div>
+    </>
+  );
+};
+
+export default GetStarted;
