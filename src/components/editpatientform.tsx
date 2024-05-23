1ef2f72191480f2d233f6b2fb3e0b4dbe1947c97
memojpinkus <memojpinkus@gmail.com>
2024-05-22 21:04:18 -0700

    CSS and Responsiveness
    Added CSS and responsiveness to some pages

--- src/components/editpatientform.tsx
+++ src/components/editpatientform.tsx
@@ -40,28 +40,43 @@ export default function EditPatientForm({id, disease, name, phone}) {
         <span className="font-bold text-4xl">Edit Patient</span>
 
         <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
-          <input 
-            onChange={e => setNewDisease(e.target.value)}
-            value={newDisease}
-            className='border border-slate-500 px-8 py-2' 
-            type='text' 
-            placeholder='Possible Disease'
-          />
-          <input 
-            onChange={e => setNewName(e.target.value)}
-            value={newName}
-            className='border border-slate-500 px-8 py-2' 
-            type='text' 
-            placeholder='Patient Name'
-          />
-          <input
-            onChange={e => setNewPhone(e.target.value)}
-            value={newPhone}
-            className='border border-slate-500 px-8 py-2' 
-            type='text' 
-            placeholder='Phone Number'
-          />
-          <button className='bg-blue-600 font-bolt text-white py-3 px-6 w-fit'>Save</button>
+          <div>
+
+            <div className="mb-6">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Possible Disease</label>
+              <input 
+                onChange={e => setNewDisease(e.target.value)}
+                value={newDisease}
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' 
+                type='text' 
+                placeholder='Possible Disease'
+              />
+            </div>
+            
+            <div className="mb-6">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Patient Name</label>
+              <input 
+                onChange={e => setNewName(e.target.value)}
+                value={newName}
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' 
+                type='text' 
+                placeholder='Patient Name'
+              />
+            </div>
+            
+            <div className="mb-6">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Phone</label>
+              <input
+                onChange={e => setNewPhone(e.target.value)}
+                value={newPhone}
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' 
+                type='text' 
+                placeholder='Phone Number'
+              />
+            </div>
+            
+            <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Save</button>
+          </div>
         </form>
       </>
     )
