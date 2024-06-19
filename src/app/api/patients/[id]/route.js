613eacbb956582293eaf3350017e036fdc6eb343
memojpinkus <memojpinkus@gmail.com>
2024-06-18 21:37:16 -0700

    MongoDB New Params
    Added new parameters for data base

--- src/app/api/patients/[id]/route.js
+++ src/app/api/patients/[id]/route.js
@@ -5,12 +5,50 @@ import Patient from "../../../../../models/patient";
 export async function PUT(request, { params }) {
   const { id } = params;
   const {
-    newDisease: disease,
     newName: name,
     newPhone: phone,
+    newApellidoPaterno: apellidoPaterno, 
+    newApellidoMaterno: apellidoMaterno, 
+    newFechaDeNacimiento: fechaDeNacimiento, 
+    newLugarDeNacimiento: lugarDeNacimiento, 
+    newEstatura: estatura, 
+    newPeso: peso, 
+    newCurp: curp, 
+    newOcupacion: ocupacion, 
+    newCalle: calle, 
+    newCiudad: ciudad,  
+    newEstado: estado, 
+    newColonia: colonia, 
+    newCodigoPostal: codigoPostal, 
+    newEmail: email, 
+    newNombreFamiliar: nombreFamiliar, 
+    newPhoneFamiliar: phoneFamiliar,
+    newEmailFamiliar: emailFamiliar, 
+    newEstadoCivil: estadoCivil, 
+    newGenero: genero, 
+    newEducacion: educacion, 
+    newTabaco: tabaco, 
+    newAlcohol: alcohol, 
+    newDrogas: drogas, 
+    newActividad: actividad, 
+    newEnfermedadCronica: enfermedadCronica,
+    newAlergias: alergias,
+    newCirugias: cirugias, 
+    newTrastornos: trastornos,
+    newCancer: cancer,
+    newHipertension: hipertension, 
+    newDiabetes: diabetes, 
+    newCancerF: cancerF, 
+    newAsma: asma,
+    newEnfermedadN: enfermedadN, 
   } = await request.json();
+
   await connectMongoDB();
-  await Patient.findByIdAndUpdate(id, { disease, name, phone });
+
+  await Patient.findByIdAndUpdate(id, { 
+    name, phone, apellidoPaterno, apellidoMaterno, fechaDeNacimiento, lugarDeNacimiento, estatura, peso, curp, ocupacion, calle, ciudad, estado, colonia, codigoPostal, email, nombreFamiliar, phoneFamiliar, emailFamiliar, estadoCivil, genero, educacion, tabaco, alcohol, drogas, actividad, enfermedadCronica, alergias, cirugias, trastornos, cancer, hipertension, diabetes, cancerF, asma, enfermedadN 
+  });
+  
   return NextResponse.json({ message: "Patient updated" }, { status: 200 });
 }
 
--- src/app/api/patients/route.js
+++ src/app/api/patients/route.js
@@ -3,9 +3,16 @@ import Patient from "../../../../models/patient";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const {disease, name, phone} = await request.json();
+    const {
+        name, phone, apellidoPaterno, apellidoMaterno, fechaDeNacimiento, lugarDeNacimiento, estatura, peso, curp, ocupacion, calle, ciudad, estado, colonia, codigoPostal, email, nombreFamiliar, phoneFamiliar, emailFamiliar, estadoCivil, genero, educacion, tabaco, alcohol, drogas, actividad, enfermedadCronica, alergias, cirugias, trastornos, cancer, hipertension, diabetes, cancerF, asma, enfermedadN
+    } = await request.json();
+
     await connectMongoDB();
-    await Patient.create({disease, name, phone});
+
+    await Patient.create({
+        name, phone, apellidoPaterno, apellidoMaterno, fechaDeNacimiento, lugarDeNacimiento, estatura, peso, curp, ocupacion, calle, ciudad, estado, colonia, codigoPostal, email, nombreFamiliar, phoneFamiliar, emailFamiliar, estadoCivil, genero, educacion, tabaco, alcohol, drogas, actividad, enfermedadCronica, alergias, cirugias, trastornos, cancer, hipertension, diabetes, cancerF, asma, enfermedadN
+    });
+    
     return NextResponse.json({message: "Patient Created"}, {status:201});
 }
 
